7b879394ab0507dfa325b30ca3f64392526e7dd0
deepakkumar-nexg <deepakkumar@nexgeniots.com>
2023-05-02 15:26:16 +0530

    implement object using TypeScript

---
+++ object.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'deepak',
+    age: 23,
+    email: 'abc@nex.com'
+};
+var user = {
+    name: 'deepak',
+    age: 23,
+    email: 'abc@nex.com'
+};
+console.log(user.name);
---
+++ object.ts
@@ -0,0 +1,17 @@
+type objType = {name: string, age: number, email: string}
+
+
+const person: objType = {
+    name: 'deepak',
+    age: 23,
+    email: 'abc@nex.com'
+}
+
+const user: objType = {
+    name: 'deepak',
+    age: 23,
+    email: 'abc@nex.com'
+}
+
+console.log(user.name);
+
