9e9b73f0e657c50ea1e5910950fd3bc90df9e22f
deepakkumar-nexg <deepakkumar@nexgeniots.com>
2023-05-02 15:27:40 +0530

    implement tuples using TypeScript

---
+++ tuple.js
@@ -0,0 +1,5 @@
+// let role:string[] |number[] = ['admin', 'manager'];
+// let role: number[] |string[] = ['admin', 1, 3]
+var role = ['admin', 'manager', 1, 2];
+role.push(true);
+role[1] = "user";
---
+++ tuple.ts
@@ -0,0 +1,7 @@
+// let role:string[] |number[] = ['admin', 'manager'];
+
+// let role: number[] |string[] = ['admin', 1, 3]
+
+let role: [string, string, number, number, boolean?] = ['admin', 'manager', 1, 2];
+role.push(true);
+role[1] = "user";
