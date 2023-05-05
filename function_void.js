6a830d9f7a3237695e1b32b0da640767687d7d8a
deepakkumar-nexg <deepakkumar@nexgeniots.com>
2023-05-05 12:59:51 +0530

    function void in typescript

---
+++ function_void.js
@@ -0,0 +1,9 @@
+//by default function is void
+var today;
+var x = 20;
+var y = 10;
+function printData() {
+    // console.log("hello");
+    today = new Date;
+    x + y;
+}
---
+++ function_void.ts
@@ -0,0 +1,17 @@
+//by default function is void
+
+let today;
+
+let x = 20;
+let y = 10;
+
+function printData() {
+
+    // console.log("hello");
+    
+    today = new Date;
+    x + y;
+    
+}
+
+
