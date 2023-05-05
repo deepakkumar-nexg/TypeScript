a829d8f21090a470661c25302301219147cf5f07
deepakkumar-nexg <deepakkumar@nexgeniots.com>
2023-05-05 12:59:00 +0530

    function type in typescript

---
+++ function_type.js
@@ -0,0 +1,15 @@
+//function type - type of value return from function is known as function type
+//why ?? - function cannot return wrong type value
+// function add(a: number, b: number): number{
+//     return a + b;
+// }
+// add(10, 20);
+var a = 20;
+var b = "hello";
+var c = undefined;
+function add() {
+    // return b;
+    // return a;
+    return c;
+}
+add();
---
+++ function_type.ts
@@ -0,0 +1,21 @@
+//function type - type of value return from function is known as function type
+//why ?? - function cannot return wrong type value
+
+// function add(a: number, b: number): number{
+//     return a + b;
+// }
+
+// add(10, 20);
+
+const a = 20;
+const b = "hello";
+const c = undefined;
+
+
+function add(): undefined {
+    // return b;
+    // return a;
+    return c;
+}
+
+add();
