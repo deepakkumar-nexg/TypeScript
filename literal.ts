0b8d11d9cf4f957790cf883d6dcc8eebd622f961
deepakkumar-nexg <deepakkumar@nexgeniots.com>
2023-05-05 13:02:56 +0530

    literal in typescript

---
+++ literal.ts
@@ -0,0 +1,13 @@
+let data : number | string = 10;
+
+
+function combine(a: number | string, b: number | string){
+    if(typeof a === 'number' && typeof b === 'number')
+        return a + b;
+    else 
+        return a.toString() + b.toString();    
+}
+
+console.log(combine(10, 4));
+console.log(combine("deepak","kumar"));
+
